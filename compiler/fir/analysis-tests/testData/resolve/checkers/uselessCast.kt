Deenu488/kotlin

commit b6adcf96be6f4343c2881f4bd23c49f16b7d5a7a

Cleanup FirHelpers.kt

compiler/fir/checkers/src/org/jetbrains/kotlin/fir/analysis/checkers/FirHelpers.kt
@@ -557,7 +557,9 @@ fun checkTypeMismatch(
 
 internal fun checkCondition(condition: FirExpression, context: CheckerContext, reporter: DiagnosticReporter) {
     val coneType = condition.resolvedType.fullyExpandedType(context.session).lowerBoundIfFlexible()
-    if (coneType !is ConeErrorType && !coneType.isSubtypeOf(context.session.typeContext, context.session.builtinTypes.booleanType.coneType)) {
+    if (coneType !is ConeErrorType &&
+        !coneType.isSubtypeOf(context.session.typeContext, context.session.builtinTypes.booleanType.coneType)
+    ) {
         reporter.reportOn(
             condition.source,
             FirErrors.CONDITION_TYPE_MISMATCH,
@@ -631,9 +633,7 @@ fun FirFunctionSymbol<*>.isFunctionForExpectTypeFromCastFeature(): Boolean {
     fun FirTypeRef.isBadType() =
         coneType.contains { (it.lowerBoundIfFlexible() as? ConeTypeParameterType)?.lookupTag == typeParameterSymbol.toLookupTag() }
 
-    if (valueParameterSymbols.any { it.resolvedReturnTypeRef.isBadType() } || resolvedReceiverTypeRef?.isBadType() == true) return false
-
-    return true
+    return valueParameterSymbols.none { it.resolvedReturnTypeRef.isBadType() } || resolvedReceiverTypeRef?.isBadType() == true
 }
 
 private val FirCallableDeclaration.isMember get() = dispatchReceiverType != null
@@ -786,18 +786,6 @@ fun FirNamedFunctionSymbol.directOverriddenFunctions(session: FirSession, scopeS
 
 fun FirNamedFunctionSymbol.directOverriddenFunctions(context: CheckerContext) =
     directOverriddenFunctions(context.session, context.sessionHolder.scopeSession)
-
-inline fun <C : MutableCollection<FirNamedFunctionSymbol>> FirNamedFunctionSymbol.collectOverriddenFunctionsWhere(
-    collection: C,
-    context: CheckerContext,
-    crossinline condition: (FirNamedFunctionSymbol) -> Boolean,
-) = collection.apply {
-    processOverriddenFunctions(context) {
-        if (condition(it)) {
-            add(it)
-        }
-    }
-}
 
 inline fun FirNamedFunctionSymbol.processOverriddenFunctions(
     context: CheckerContext,

commit 6dd7ae20595b33e0cc34bea43c680cfdb8ea2c75

[FIR] Handle context parameters in useless cast detection
This also fixes a regression introduced in
b6adcf96be6f4343c2881f4bd23c49f16b7d5a7a where the check was incorrectly
changed from && to || and covers it with a test.

#KT-75160

compiler/fir/checkers/src/org/jetbrains/kotlin/fir/analysis/checkers/FirHelpers.kt
@@ -699,7 +699,9 @@ fun FirFunctionSymbol<*>.isFunctionForExpectTypeFromCastFeature(): Boolean {
     fun FirTypeRef.isBadType() =
         coneType.contains { (it.lowerBoundIfFlexible() as? ConeTypeParameterType)?.lookupTag == typeParameterSymbol.toLookupTag() }
 
-    return valueParameterSymbols.none { it.resolvedReturnTypeRef.isBadType() } || resolvedReceiverTypeRef?.isBadType() == true
+    return valueParameterSymbols.none { it.resolvedReturnTypeRef.isBadType() }
+            && resolvedReceiverTypeRef?.isBadType() != true
+            && resolvedContextParameters.none { it.returnTypeRef.isBadType() }
 }
 
 private val FirCallableDeclaration.isMember get() = dispatchReceiverType != null

analysis/low-level-api-fir/tests/org/jetbrains/kotlin/analysis/low/level/api/fir/diagnostic/compiler/based/LLDiagnosticsFirTestGenerated.java
@@ -1630,6 +1630,12 @@ public class LLDiagnosticsFirTestGenerated extends AbstractLLDiagnosticsTest {
       }
 
       @Test
+      @TestMetadata("uselessCast.kt")
+      public void testUselessCast() {
+        runTest("compiler/fir/analysis-tests/testData/resolve/checkers/uselessCast.kt");
+      }
+
+      @Test
       @TestMetadata("uselessCastLeadsToRecursiveProblem.kt")
       public void testUselessCastLeadsToRecursiveProblem() {
         runTest("compiler/fir/analysis-tests/testData/resolve/checkers/uselessCastLeadsToRecursiveProblem.kt");

analysis/low-level-api-fir/tests/org/jetbrains/kotlin/analysis/low/level/api/fir/diagnostic/compiler/based/LLReversedDiagnosticsFirTestGenerated.java
@@ -1630,6 +1630,12 @@ public class LLReversedDiagnosticsFirTestGenerated extends AbstractLLReversedDia
       }
 
       @Test
+      @TestMetadata("uselessCast.kt")
+      public void testUselessCast() {
+        runTest("compiler/fir/analysis-tests/testData/resolve/checkers/uselessCast.kt");
+      }
+
+      @Test
       @TestMetadata("uselessCastLeadsToRecursiveProblem.kt")
       public void testUselessCastLeadsToRecursiveProblem() {
         runTest("compiler/fir/analysis-tests/testData/resolve/checkers/uselessCastLeadsToRecursiveProblem.kt");

compiler/fir/analysis-tests/testData/resolve/checkers/uselessCast.kt
@@ -1,0 +1,34 @@
+// RUN_PIPELINE_TILL: BACKEND
+// LANGUAGE: +ContextParameters
+
+fun <T> foo(a: T) = a
+
+context(a: T)
+fun <T> foo2() = a
+
+fun <T> T.foo3() = this
+
+fun <T> T.foo4(a: T) = a
+
+context(a: T)
+fun <T> T.foo5(a2: T) = a
+
+fun <T> T.test(): T {
+    return foo(this) <!USELESS_CAST!>as T<!>
+}
+
+fun <T> T.test2(): T {
+    return foo2() <!USELESS_CAST!>as T<!>
+}
+
+fun <T> T.test3(): T {
+    return foo3() <!USELESS_CAST!>as T<!>
+}
+
+fun <T> T.test4(): T {
+    return foo4(this) <!USELESS_CAST!>as T<!>
+}
+
+fun <T> T.test5(): T {
+    return foo5(this) <!USELESS_CAST!>as T<!>
+}

compiler/fir/analysis-tests/tests-gen/org/jetbrains/kotlin/test/runners/FirLightTreeDiagnosticsWithLatestLanguageVersionTestGenerated.java
@@ -1630,6 +1630,12 @@ public class FirLightTreeDiagnosticsWithLatestLanguageVersionTestGenerated exten
       }
 
       @Test
+      @TestMetadata("uselessCast.kt")
+      public void testUselessCast() {
+        runTest("compiler/fir/analysis-tests/testData/resolve/checkers/uselessCast.kt");
+      }
+
+      @Test
       @TestMetadata("uselessCastLeadsToRecursiveProblem.kt")
       public void testUselessCastLeadsToRecursiveProblem() {
         runTest("compiler/fir/analysis-tests/testData/resolve/checkers/uselessCastLeadsToRecursiveProblem.kt");

compiler/fir/analysis-tests/tests-gen/org/jetbrains/kotlin/test/runners/PhasedJvmDiagnosticLightTreeTestGenerated.java
@@ -51378,6 +51378,12 @@ public class PhasedJvmDiagnosticLightTreeTestGenerated extends AbstractPhasedJvm
       }
 
       @Test
+      @TestMetadata("uselessCast.kt")
+      public void testUselessCast() {
+        runTest("compiler/fir/analysis-tests/testData/resolve/checkers/uselessCast.kt");
+      }
+
+      @Test
       @TestMetadata("uselessCastLeadsToRecursiveProblem.kt")
       public void testUselessCastLeadsToRecursiveProblem() {
         runTest("compiler/fir/analysis-tests/testData/resolve/checkers/uselessCastLeadsToRecursiveProblem.kt");

compiler/fir/analysis-tests/tests-gen/org/jetbrains/kotlin/test/runners/PhasedJvmDiagnosticPsiTestGenerated.java
@@ -51533,6 +51533,12 @@ public class PhasedJvmDiagnosticPsiTestGenerated extends AbstractPhasedJvmDiagno
       }
 
       @Test
+      @TestMetadata("uselessCast.kt")
+      public void testUselessCast() {
+        runTest("compiler/fir/analysis-tests/testData/resolve/checkers/uselessCast.kt");
+      }
+
+      @Test
       @TestMetadata("uselessCastLeadsToRecursiveProblem.kt")
       public void testUselessCastLeadsToRecursiveProblem() {
         runTest("compiler/fir/analysis-tests/testData/resolve/checkers/uselessCastLeadsToRecursiveProblem.kt");