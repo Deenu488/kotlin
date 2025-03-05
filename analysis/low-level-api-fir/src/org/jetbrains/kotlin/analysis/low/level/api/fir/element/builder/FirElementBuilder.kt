b0f25a82191a72b73b318390442b8808d247adae
Alexey Belkov <alexey.belkov@jetbrains.com>
2025-03-05 14:51:07 +0400

    [LL] Rename variable in getFirForNonKtFileElement

--- analysis/low-level-api-fir/src/org/jetbrains/kotlin/analysis/low/level/api/fir/element/builder/FirElementBuilder.kt
+++ analysis/low-level-api-fir/src/org/jetbrains/kotlin/analysis/low/level/api/fir/element/builder/FirElementBuilder.kt
@@ -117,8 +117,8 @@ internal class FirElementBuilder(private val moduleComponents: LLFirModuleResolv
         }
 
         val psi = getPsiAsFirElementSource(element) ?: return null
-        val firFile = element.containingKtFile
-        val fileStructure = moduleComponents.fileStructureCache.getFileStructure(firFile)
+        val ktFile = element.containingKtFile
+        val fileStructure = moduleComponents.fileStructureCache.getFileStructure(ktFile)
 
         val structureElement = fileStructure.getStructureElementFor(element, nonLocalContainer)
         val mappings = structureElement.mappings
